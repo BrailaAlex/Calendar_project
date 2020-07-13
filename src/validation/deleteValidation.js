01de6b9fa9e1c0db42cef3cdfd91c00a6c3a8fac
BrailaAlex <60700132+BrailaAlex@users.noreply.github.com>
2020-07-13 19:35:04 +0300

    Update deleteValidation.js

--- src/validation/deleteValidation.js
+++ src/validation/deleteValidation.js
@@ -9,7 +9,7 @@ export const deleteValidation = (startTime, date) => {
 
   if (beforeDiff < 15 && beforeDiff >= 0) {
     alert(
-      "You can't skip the event, when it less than 15 mins before it'll start and before it'll end"
+      "You can't skip the event, when it less than 15 mins before it'll start"
     );
     return true;
   } else {
